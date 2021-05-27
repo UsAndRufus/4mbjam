24dcce8a909d5425087036ec19ff5c49fb78c832
Edward B <edward.bennigsen@protonmail.com>
2021-05-27 22:35:10 +0100

    sidebar

--- main.c
+++ main.c
@@ -1,11 +1,25 @@
 #include "raylib.h"
 
+// Board
 #define CELLS 6
 #define TOTAL_CELLS (CELLS * CELLS)
 #define CELL_SIZE 100
 #define BOARD_SIZE ((CELLS * CELL_SIZE) + (CELLS + 1))
 #define BOARD_BOUNDARY (BOARD_SIZE + BORDER)
 #define BORDER 20
+
+// Sidebar
+#define SIDEBAR_WIDTH 200
+#define SIDEBAR_HEIGHT BOARD_SIZE
+#define SIDEBAR_X (BOARD_BOUNDARY + BORDER)
+#define SIDEBAR_Y BORDER
+#define PADDING 10
+#define SIDEBAR_INNER_X (SIDEBAR_X + PADDING)
+#define SIDEBAR_INNER_Y (SIDEBAR_Y + PADDING)
+
+// Window
+#define WINDOW_WIDTH (BOARD_SIZE + BORDER*3 + SIDEBAR_WIDTH)
+#define WINDOW_HEIGHT (BOARD_SIZE + BORDER*2)
 
 void drawGrid() {
     for (int i = BORDER; i <= BOARD_BOUNDARY; i += CELL_SIZE + 1) {
@@ -19,7 +33,7 @@ int main(void) {
     // Initialization
     //--------------------------------------------------------------------------------------
 
-    InitWindow(BOARD_SIZE + BORDER*2, BOARD_SIZE + BORDER*2, "4mb game");
+    InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "4mb game");
 
     Rectangle cellRecs[TOTAL_CELLS] = { 0 };     // Rectangles array
 
@@ -60,6 +74,7 @@ int main(void) {
 
             ClearBackground(DARKBLUE);
 
+            // Board
             drawGrid();
             
             for (int i = 0; i < TOTAL_CELLS; i++) {
@@ -69,7 +84,10 @@ int main(void) {
                 }
             }
             
-            DrawFPS(1,1);
+            // Sidebar
+            DrawRectangle(SIDEBAR_X, SIDEBAR_Y, SIDEBAR_WIDTH, SIDEBAR_HEIGHT, SKYBLUE);
+            
+            DrawFPS(SIDEBAR_INNER_X,SIDEBAR_INNER_Y);
             
 
         EndDrawing();
