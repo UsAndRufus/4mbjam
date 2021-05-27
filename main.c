fe12020fd35188fa414b6de2c187f044f368f0c3
Edward B <edward.bennigsen@protonmail.com>
2021-05-27 22:24:25 +0100

    Style/naming

--- main.c
+++ main.c
@@ -5,7 +5,7 @@
 #define CELL_SIZE 100
 #define BOARD_SIZE ((CELLS * CELL_SIZE) + (CELLS + 1))
 #define BOARD_BOUNDARY (BOARD_SIZE + BORDER)
-#define BORDER 10
+#define BORDER 20
 
 void drawGrid() {
     for (int i = BORDER; i <= BOARD_BOUNDARY; i += CELL_SIZE + 1) {
@@ -15,25 +15,23 @@ void drawGrid() {
 }
 
 
-int main(void)
-{
+int main(void) {
     // Initialization
     //--------------------------------------------------------------------------------------
 
     InitWindow(BOARD_SIZE + BORDER*2, BOARD_SIZE + BORDER*2, "4mb game");
 
-    Rectangle colorsRecs[TOTAL_CELLS] = { 0 };     // Rectangles array
+    Rectangle cellRecs[TOTAL_CELLS] = { 0 };     // Rectangles array
 
-    // Fills colorsRecs data (for every rectangle)
-    for (int i = 0; i < TOTAL_CELLS; i++)
-    {
-        colorsRecs[i].x = BORDER + CELL_SIZE * (i % CELLS) + (i % CELLS);
-        colorsRecs[i].y = BORDER + 1 + CELL_SIZE * (i / CELLS) + (i / CELLS);
-        colorsRecs[i].width = CELL_SIZE;
-        colorsRecs[i].height = CELL_SIZE;
+    // Fills cellRecs data (for every rectangle)
+    for (int i = 0; i < TOTAL_CELLS; i++) {
+        cellRecs[i].x = BORDER + CELL_SIZE * (i % CELLS) + (i % CELLS);
+        cellRecs[i].y = BORDER + 1 + CELL_SIZE * (i / CELLS) + (i / CELLS);
+        cellRecs[i].width = CELL_SIZE;
+        cellRecs[i].height = CELL_SIZE;
     }
     
-    int colorState[TOTAL_CELLS] = { 0 };           // Color state: 0-DEFAULT, 1-MOUSE_HOVER
+    int cellState[TOTAL_CELLS] = { 0 };           // Cell state: 0-DEFAULT, 1-MOUSE_HOVER
 
     Vector2 mousePoint = { 0.0f, 0.0f };
 
@@ -41,17 +39,15 @@ int main(void)
     //---------------------------------------------------------------------------------------
 
     // Main game loop
-    while (!WindowShouldClose())    // Detect window close button or ESC key
-    {
+    while (!WindowShouldClose()) {
         // Update
         //----------------------------------------------------------------------------------
 
         mousePoint = GetMousePosition();
 
-        for (int i = 0; i < TOTAL_CELLS; i++)
-        {
-            if (CheckCollisionPointRec(mousePoint, colorsRecs[i])) colorState[i] = 1;
-            else colorState[i] = 0;
+        for (int i = 0; i < TOTAL_CELLS; i++) {
+            if (CheckCollisionPointRec(mousePoint, cellRecs[i])) cellState[i] = 1;
+            else cellState[i] = 0;
         }
         
         // printf("x: %f; y: %f\n", mousePoint.x, mousePoint.y);
@@ -66,13 +62,14 @@ int main(void)
 
             drawGrid();
             
-            for (int i = 0; i < TOTAL_CELLS; i++) 
-            {
-                if (colorState[i])
+            for (int i = 0; i < TOTAL_CELLS; i++) {
+                if (cellState[i])
                 {
-                    DrawRectangleRec(colorsRecs[i], LIME);
+                    DrawRectangleRec(cellRecs[i], LIME);
                 }
             }
+            
+            DrawFPS(1,1);
             
 
         EndDrawing();
