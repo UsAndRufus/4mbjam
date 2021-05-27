84c2a54e9cddc9a035ce0727b9c7e3efbffe0e79
Edward B <edward.bennigsen@protonmail.com>
2021-05-27 22:00:34 +0100

    Highlight cells on the grid

--- main.c
+++ main.c
@@ -1,15 +1,16 @@
 #include "raylib.h"
 
 #define CELLS 6
+#define TOTAL_CELLS (CELLS * CELLS)
 #define CELL_SIZE 100
-#define BOARD_SIZE ((CELLS * CELL_SIZE) + (CELLS * 1))
+#define BOARD_SIZE ((CELLS * CELL_SIZE) + (CELLS + 1))
 #define BOARD_BOUNDARY (BOARD_SIZE + BORDER)
 #define BORDER 10
 
 void drawGrid() {
     for (int i = BORDER; i <= BOARD_BOUNDARY; i += CELL_SIZE + 1) {
         DrawLine(i, BORDER, i, BOARD_BOUNDARY, SKYBLUE);
-        DrawLine(BORDER, i, BOARD_BOUNDARY, i, SKYBLUE);
+        DrawLine(BORDER, i, BOARD_BOUNDARY - 1, i, SKYBLUE);
     }
 }
 
@@ -21,6 +22,21 @@ int main(void)
 
     InitWindow(BOARD_SIZE + BORDER*2, BOARD_SIZE + BORDER*2, "4mb game");
 
+    Rectangle colorsRecs[TOTAL_CELLS] = { 0 };     // Rectangles array
+
+    // Fills colorsRecs data (for every rectangle)
+    for (int i = 0; i < TOTAL_CELLS; i++)
+    {
+        colorsRecs[i].x = BORDER + CELL_SIZE * (i % CELLS) + (i % CELLS);
+        colorsRecs[i].y = BORDER + 1 + CELL_SIZE * (i / CELLS) + (i / CELLS);
+        colorsRecs[i].width = CELL_SIZE;
+        colorsRecs[i].height = CELL_SIZE;
+    }
+    
+    int colorState[TOTAL_CELLS] = { 0 };           // Color state: 0-DEFAULT, 1-MOUSE_HOVER
+
+    Vector2 mousePoint = { 0.0f, 0.0f };
+
     SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
     //---------------------------------------------------------------------------------------
 
@@ -29,6 +45,16 @@ int main(void)
     {
         // Update
         //----------------------------------------------------------------------------------
+
+        mousePoint = GetMousePosition();
+
+        for (int i = 0; i < TOTAL_CELLS; i++)
+        {
+            if (CheckCollisionPointRec(mousePoint, colorsRecs[i])) colorState[i] = 1;
+            else colorState[i] = 0;
+        }
+        
+        // printf("x: %f; y: %f\n", mousePoint.x, mousePoint.y);
 
         //----------------------------------------------------------------------------------
 
@@ -39,6 +65,15 @@ int main(void)
             ClearBackground(DARKBLUE);
 
             drawGrid();
+            
+            for (int i = 0; i < TOTAL_CELLS; i++) 
+            {
+                if (colorState[i])
+                {
+                    DrawRectangleRec(colorsRecs[i], LIME);
+                }
+            }
+            
 
         EndDrawing();
         //----------------------------------------------------------------------------------
